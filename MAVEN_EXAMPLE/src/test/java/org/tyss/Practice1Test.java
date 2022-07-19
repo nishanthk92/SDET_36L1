e9926218683de4b49ebb1d36ab808c6324c9c84f
NK Achar <NK Achar@Admin>
2022-07-19 11:44:13 +0530

    first_commit

---
+++ MAVEN_EXAMPLE/src/test/java/org/tyss/Practice1Test.java
@@ -0,0 +1,29 @@
+package org.tyss;
+
+import org.testng.annotations.Test;
+
+public class Practice1Test 
+{
+@Test
+public void practice1test()
+{
+	String BROWSER = System.getProperty("browser");
+	String USERNAME = System.getProperty("username");
+	String PASSWORD = System.getProperty("password");
+}
+@Test
+public void practice2test()
+{
+	System.out.println("TYSS1---> Test1");
+}
+@Test
+public void practice3test()
+{
+	System.out.println("TYSS1---> Test1");
+}
+@Test
+public void practice4test()
+{
+System.out.println("TYSS1---> Test1");
+}
+}
