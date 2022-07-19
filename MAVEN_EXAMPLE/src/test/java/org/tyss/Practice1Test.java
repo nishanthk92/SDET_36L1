cf4361474f04d0fe34b0f4f96a858472acbe32a7
NK Achar <NK Achar@Admin>
2022-07-19 12:18:11 +0530

    2nd commit

--- MAVEN_EXAMPLE/src/test/java/org/tyss/Practice1Test.java
+++ MAVEN_EXAMPLE/src/test/java/org/tyss/Practice1Test.java
@@ -5,13 +5,6 @@ import org.testng.annotations.Test;
 public class Practice1Test 
 {
 @Test
-public void practice1test()
-{
-	String BROWSER = System.getProperty("browser");
-	String USERNAME = System.getProperty("username");
-	String PASSWORD = System.getProperty("password");
-}
-@Test
 public void practice2test()
 {
 	System.out.println("TYSS1---> Test1");
